dedb02b3938918686620e0bd3d7b82e50210d697
rxdps93 <dpsirott@gmail.com>
2023-06-13 16:15:31 -0400

    d2p2 complete

--- day02/d2.py
+++ day02/d2.py
@@ -1,6 +1,21 @@
-input = [list(map(int, num)) for num in[row.split() for row in open('input', 'r').read().splitlines()]]
+input = [sorted(list(map(int, num))) for num in[row.split() for row in open('test2', 'r').read().splitlines()]]
 
-def part1(input):
-    return sum(int(max(row)) - int(min(row)) for row in input)
+def part1():
+    return sum(max(row) - min(row) for row in input)
 
-print(part1(input))
+def part2():
+    total = 0
+    for row in input:
+        loop = True
+        for lo in row:
+            for num in row[row.index(lo) + 1:]:
+                if num % lo == 0:
+                    total += int(num / lo)
+                    loop = False
+                    break
+            if loop == False:
+                break
+    return total
+
+print(part1())
+print(part2())
