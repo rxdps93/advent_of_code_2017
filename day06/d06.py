4ec50ca67dc437a5ac60d0c6375873cdf8a9af11
rxdps93 <dpsirott@gmail.com>
2023-06-14 15:11:33 -0400

    d6p2 complete

--- day06/d06.py
+++ day06/d06.py
@@ -15,4 +15,20 @@ def part1():
 
     return len(states)
 
+def part2():
+    states = set()
+    state = tuple(input)
+    while state not in states:
+        states.add(state)
+        state = list(state)
+        num = max(state)
+        idx = state.index(num)
+        state[idx] = 0
+        for i in range(1, num + 1):
+            state[(idx + i) % len(state)] += 1
+        state = tuple(state)
+
+    return len(states) - list(states).index(state)
+
 print(part1())
+print(part2())
