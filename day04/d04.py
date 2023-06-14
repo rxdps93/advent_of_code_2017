48b4292b13266e0971f9bf21a8b228234f283cb3
rxdps93 <dpsirott@gmail.com>
2023-06-14 14:08:37 -0400

    one liner

--- day04/d04.py
+++ day04/d04.py
@@ -4,14 +4,7 @@ def part1():
     return [len(row[0]) == len(row[1]) for row in zip(input, [set(row) for row in input])].count(True)
 
 def part2():
-    srt = []
-    lst = []
-    for row in input:
-        lst = []
-        for word in row:
-            lst.append(''.join(sorted(word)))
-        srt += [list(set(lst))]
-    return [len(row[0]) == len(row[1]) for row in zip(input, srt)].count(True)
+    return [len(row[0]) == len(row[1]) for row in zip(input, [list({''.join(sorted(word)) for word in row}) for row in input])].count(True)
 
 print(part1())
 print(part2())
